d827d5cf40f21e1c05562420c0a9fcd50073d9ba
cattanach-mfld <53875551+cattanach-mfld@users.noreply.github.com>
2021-05-07 13:48:32 -0500

    Add files via upload

--- CustomOSD.ps1
+++ CustomOSD.ps1
@@ -9,66 +9,7 @@ Start-Sleep -Seconds 1
 #Import-Module OSD -Force
 
 #Set Dell BIOS
-Function Get-DellBIOSProvider
-{
-    [CmdletBinding()]
-    param()		
-	If (!(Get-Module DellBIOSProvider -listavailable)) 
-		{
-			Install-Module DellBIOSProvider -ErrorAction SilentlyContinue
-			Write_Log -Message_Type "INFO" -Message "DellBIOSProvider has been installed"  			
-		}
-	Else
-		{
-			Import-Module DellBIOSProvider -ErrorAction SilentlyContinue
-			Write_Log -Message_Type "INFO" -Message "DellBIOSProvider has been imported"  			
-		}
-}
 
-Get-DellBIOSProvider 
-  
-$IsPasswordSet = (Get-Item -Path DellSmbios:\Security\IsAdminPasswordSet).currentvalue 
-
-If($IsPasswordSet -eq $false) {
-    $passwd = Read-Host -AsSecureString 'Enter BIOS Password'
-    & Set-Item -Path Dellsmbios:\Security\AdminPassword -Value ((New-Object System.Management.Automation.PSCredential('dummy',$passwd)).GetNetworkCredential().Password)     
-
-    $settingsList = @() # Create the empty array that will eventually be the CSV file
-
-    $row = New-Object System.Object # Create an object to append to the array
-    $row | Add-Member -MemberType NoteProperty -Name "Setting" -Value "AdminSetupLockout"
-    $row | Add-Member -MemberType NoteProperty -Name "Value" -Value "Enabled"
-    $settingsList += $row
-    $row = New-Object System.Object # Create an object to append to the array
-    $row | Add-Member -MemberType NoteProperty -Name "Setting" -Value "LegacyOrom"
-    $row | Add-Member -MemberType NoteProperty -Name "Value" -Value "Disabled"
-    $settingsList += $row
-    $row = New-Object System.Object # Create an object to append to the array
-    $row | Add-Member -MemberType NoteProperty -Name "Setting" -Value "UefiBootPathSecurity"
-    $row | Add-Member -MemberType NoteProperty -Name "Value" -Value "AlwaysExceptInternalHdd"
-    $settingsList += $row
-
-    $Dell_BIOS = get-childitem -path DellSmbios:\ | foreach {
-        get-childitem -path @("DellSmbios:\" + $_.Category)  | select-object attribute, currentvalue, possiblevalues, PSChildName}   
-        
-    foreach ($New_Setting in $settingsList) { 
-        $Setting_To_Set = $New_Setting.Setting 
-        $Setting_NewValue_To_Set = $New_Setting.Value 
-        
-        ForEach($Current_Setting in $Dell_BIOS | Where {$_.attribute -eq $Setting_To_Set}) { 
-            $Attribute = $Current_Setting.attribute
-            $Setting_Cat = $Current_Setting.PSChildName
-            $Setting_Current_Value = $Current_Setting.CurrentValue
-
-            Try {
-                & Set-Item -Path Dellsmbios:\$Setting_Cat\$Attribute -Value $Setting_NewValue_To_Set -Password ((New-Object System.Management.Automation.PSCredential('dummy',$passwd)).GetNetworkCredential().Password)						
-            }
-            Catch {
-                Write-Host "ERROR1 - Can not change setting $Attribute (Return code $Change_Return_Code)"  																		
-            }
-        }  
-    }  
-}
 
 
 
