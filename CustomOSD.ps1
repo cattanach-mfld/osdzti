29cfc6129082fd97cbf5790ce2b334e5d2ae8b5f
cattanach-mfld <53875551+cattanach-mfld@users.noreply.github.com>
2021-05-07 14:15:14 -0500

    Add files via upload

--- CustomOSD.ps1
+++ CustomOSD.ps1
@@ -1,6 +1,10 @@
 Write-Host  -ForegroundColor Cyan "Starting Marshfield's Custom OSDCloud ..."
 Start-Sleep -Seconds 1
 
+#Prompt for BIOS Password
+Write-Host  -ForegroundColor Cyan "Enter BIOS Password"
+$passwd = Read-Host -AsSecureString 'Password'
+
 #Make sure I have the latest OSD Content
 #Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
 #Install-Module OSD -Force
@@ -8,18 +12,13 @@ Start-Sleep -Seconds 1
 #Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
 #Import-Module OSD -Force
 
-#Set Dell BIOS
-
-
-
-
-
-
-
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with Marshfield Parameters"
 Write-Host  -ForegroundColor Cyan "OSLanguage en-us OSBuild 20H2 OSEdition Education ZTI"
 Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Education -ZTI
+
+#Dell BIOS Config
+
 
 
 #Restart from WinPE
