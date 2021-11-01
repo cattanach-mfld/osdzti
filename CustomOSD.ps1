d044c48016db35fdee743b89bb9ddb96e03e0b69
cattanach-mfld <53875551+cattanach-mfld@users.noreply.github.com>
2021-11-01 11:43:36 -0500

    Update CustomOSD.ps1

--- CustomOSD.ps1
+++ CustomOSD.ps1
@@ -29,7 +29,8 @@ $password = ((New-Object System.Management.Automation.PSCredential('dummy',$pass
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with Marshfield Parameters"
 Write-Host  -ForegroundColor Cyan "OSLanguage en-us OSBuild 21H2 OSEdition Education ZTI"
 #Start-OSDCloud -OSVersion "Windows 11" -OSLanguage en-us -OSBuild 21H2 -OSEdition Education -ZTI
-Start-OSDCloudGUI
+#Start-OSDCloudGUI
+Start-OSDCloud -OSLanguage en-us -OSBuild 21H2 -OSEdition Education -ZTI
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 15 seconds!"
