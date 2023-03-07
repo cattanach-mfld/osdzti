4bf1d75dea0199b583e1b1d5c1e131885b10ed6b
cattanach-mfld <53875551+cattanach-mfld@users.noreply.github.com>
2023-03-07 15:15:40 -0600

    Update CustomOSD.ps1

--- CustomOSD.ps1
+++ CustomOSD.ps1
@@ -89,8 +89,8 @@ if ((Get-MyComputerManufacturer -Brief) -eq "Dell") {
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with Marshfield Parameters"
-Start-OSDCloud -ImageFileUrl $wimUrl -ZTI
-#Start-OSDCloudMFLD -OSVersion $mfldwinver -OSLanguage en-us -OSBuild 21H2 -OSEdition Education -ZTI
+#Start-OSDCloud -ImageFileUrl $wimUrl -ZTI
+Start-OSDCloudMFLD -OSVersion "Windows 11" -OSLanguage en-us -OSBuild 22H2 -OSEdition Education -ZTI
 
 ##########################################################################
 ###################### START UPDATE DELL BIOS  ###########################
